d6fcf884c00185d541c4ac60779c886118a27f39
LouieJae <Louiejae@hccci.edu.ph>
2025-08-11 08:42:22 +0800

    Deploy:

--- coffeeshop_frontend/src/lib/utils.js
+++ coffeeshop_frontend/src/lib/utils.js
@@ -1,6 +1,12 @@
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Utility function to conditionally join class names together.
+ * 
+ * @param {...any} inputs - Class names to be merged.
+ * @returns {string} - Merged class names.
+ */
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
